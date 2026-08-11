2c7a09a669f34d6b7d9785df86dcfe5fa66571b5
Sarahwillock <111887339+Sarahwillock@users.noreply.github.com>
2026-08-11 17:17:11 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
 
 import LobbyHome from './LobbyHome';
 
 import MoveLayout from './move/MoveLayout';
 import MoveHome from './move/pages/MoveHome';
 import Agenda from './move/pages/Agenda';
-import April from './move/pages/April';
-import May from './move/pages/May';
-import June from './move/pages/June';
+
+// Vamos criar essas páginas na sequência
+import Agosto from './move/pages/Agosto';
+import Setembro from './move/pages/Setembro';
+import Outubro from './move/pages/Outubro';
+import Novembro from './move/pages/Novembro';
+import Dezembro from './move/pages/Dezembro';
+
 import Leaders from './move/pages/Leaders';
 import GCs from './move/pages/GCs';
 import Local from './move/pages/Local';
@@ -16,25 +26,84 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Lobby atual */}
-        <Route path="/" element={<LobbyHome />} />
 
-        {/* Área MOVE */}
-        <Route path="/move" element={<MoveLayout />}>
-          <Route index element={<MoveHome />} />
+        {/* LOBBY PRINCIPAL */}
+        <Route
+          path="/"
+          element={<LobbyHome />}
+        />
 
-          <Route path="agenda" element={<Agenda />} />
+        {/* ÁREA MOVE */}
+        <Route
+          path="/move"
+          element={<MoveLayout />}
+        >
+          {/* Início MOVE */}
+          <Route
+            index
+            element={<MoveHome />}
+          />
 
-          <Route path="abril" element={<April />} />
-          <Route path="maio" element={<May />} />
-          <Route path="junho" element={<June />} />
+          {/* Agenda */}
+          <Route
+            path="agenda"
+            element={<Agenda />}
+          />
 
-          <Route path="leaders" element={<Leaders />} />
-          <Route path="gcs" element={<GCs />} />
-          <Route path="local" element={<Local />} />
+          {/* Meses */}
+          <Route
+            path="agosto"
+            element={<Agosto />}
+          />
+
+          <Route
+            path="setembro"
+            element={<Setembro />}
+          />
+
+          <Route
+            path="outubro"
+            element={<Outubro />}
+          />
+
+          <Route
+            path="novembro"
+            element={<Novembro />}
+          />
+
+          <Route
+            path="dezembro"
+            element={<Dezembro />}
+          />
+
+          {/* Outras áreas */}
+          <Route
+            path="leaders"
+            element={<Leaders />}
+          />
+
+          <Route
+            path="gcs"
+            element={<GCs />}
+          />
+
+          <Route
+            path="local"
+            element={<Local />}
+          />
         </Route>
 
-        <Route path="*" element={<Navigate to="/" replace />} />
+        {/* QUALQUER ROTA INVÁLIDA VOLTA PARA O LOBBY */}
+        <Route
+          path="*"
+          element={
+            <Navigate
+              to="/"
+              replace
+            />
+          }
+        />
+
       </Routes>
     </BrowserRouter>
   );
