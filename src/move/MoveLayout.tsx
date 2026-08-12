6ff5de17bae31ba430a94a8383c5b170d6bdf3a4
Sarahwillock <111887339+Sarahwillock@users.noreply.github.com>
2026-08-12 13:00:08 -0300

    Integrate moveImages into MoveLayout component
    Refactor MoveLayout component to use moveImages for background.

--- src/move/MoveLayout.tsx
+++ src/move/MoveLayout.tsx
@@ -5,30 +5,40 @@ import BottomNav from './components/BottomNav';
 import MoveHeader from './components/MoveHeader';
 import MoveBackground from './components/MoveBackground';
 
+import { moveImages } from './moveImages';
+
 export default function MoveLayout() {
   return (
-    <div className="relative min-h-screen overflow-x-hidden text-white">
-
-      {/* FUNDO COM IMAGENS DA MOVE */}
-      <MoveBackground />
+    <div
+      className="
+        relative
+        min-h-screen
+        overflow-x-hidden
+        text-white
+      "
+    >
+      {/* FUNDO */}
+      <MoveBackground images={[...moveImages]} />
 
       {/* SIDEBAR */}
       <Sidebar />
 
       {/* CONTEÚDO */}
-      <div className="relative z-10 min-w-0 lg:ml-[215px]">
-
+      <div
+        className="
+          relative z-10
+          min-w-0
+          lg:ml-[215px]
+        "
+      >
         <MoveHeader />
 
         <main className="min-h-screen pb-24 lg:pb-0">
           <Outlet />
         </main>
-
       </div>
 
-      {/* MENU MOBILE */}
       <BottomNav />
-
     </div>
   );
 }
