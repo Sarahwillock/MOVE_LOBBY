caa3cfb8fdee18514933d43a17f646adbf0d5245
Sarahwillock <111887339+Sarahwillock@users.noreply.github.com>
2026-08-12 12:47:42 -0300

    Update MoveLayout.tsx

--- src/move/MoveLayout.tsx
+++ src/move/MoveLayout.tsx
@@ -6,17 +6,22 @@ import MoveHeader from './components/MoveHeader';
 
 export default function MoveLayout() {
   return (
-    <div className="min-h-screen bg-black text-white overflow-x-hidden">
+    <div className="move-page min-h-screen overflow-x-hidden">
+      {/* MENU LATERAL - DESKTOP */}
       <Sidebar />
 
+      {/* CONTEÚDO PRINCIPAL */}
       <div className="min-w-0 lg:ml-[215px]">
+        {/* CABEÇALHO */}
         <MoveHeader />
 
+        {/* PÁGINAS */}
         <main className="min-h-screen pb-24 lg:pb-0">
           <Outlet />
         </main>
       </div>
 
+      {/* MENU INFERIOR - MOBILE */}
       <BottomNav />
     </div>
   );
